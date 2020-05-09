6e64b7bd243590e4615310dc4e4fb624fe3bb30f
Dario D'Aversa <dario@newbweb.com>
2020-05-08 23:56:46 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+import helloWorld from './lib';
+
+console.clear();
+console.log('\n--- CODE START ---\n');
+
+helloWorld(); // Replace this with your  ECMAScript 6 test code here
+
+console.log('\n---- CODE END ----\n');
---
+++ src/lib.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-console */
+export default () => {
+  console.log('Hello world!');
+};
